550dd4bf96e2c46cb145a6ae2e21ea12be6f4c77
Dharmik Patel <dptl@amazon.com>
2023-02-25 18:45:39 -0500

    add rules page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { CreateRoomComponent } from './components/create-room/create-room.compon
 import { JoinRoomComponent } from './components/join-room/join-room.component';
 import { RoomJoinedComponent } from './components/room-joined/room-joined.component';
 import { RoomComponent } from './components/room/room.component';
+import { GameRulesComponent } from './components/game-rules/game-rules.component';
 
 const routes: Routes = [
   { path: '', component: JoinRoomComponent },
   { path: 'room', component: RoomComponent },
   { path: 'create-room', component: CreateRoomComponent },
   { path: 'room-joined', component: RoomJoinedComponent },
+  { path: 'game-rules', component: GameRulesComponent}
 ];
 
 @NgModule({
